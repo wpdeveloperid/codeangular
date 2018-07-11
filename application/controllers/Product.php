15d40a3ec07c22bf75984f85cacdfd9d741f507b
Khussal "Kus" Zamlahani <zamlahani@gmail.com>
2018-07-11 13:31:03 +0700

    bikin method head dan construct

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -1,7 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Product extends CI_Controller {
+
+	public function __construct()
+        {
+                parent::__construct();
+                $this->load->model('config_model');
+                $this->load->helper('url_helper');
+        }
+        
 	public function index(){
+		//$this->load->helper('url');
 		$this->load->view('product/product_template');
+		//return '<h1>hola</h1>halo';
+	}
+	
+	public function head(){
+		$data = $this->config_model->get_head();
+		echo json_encode($data);
 	}
 }
