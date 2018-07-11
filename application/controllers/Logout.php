35ce563e85b8975c0259a2d3f03b64c7d1818193
Khussal "Kus" Zamlahani <zamlahani@gmail.com>
2018-07-12 00:08:21 +0700

    LOGOUT: hapus helper url & model user

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -5,8 +5,6 @@ class Logout extends CI_Controller {
 	public function __construct()
         {
 			parent::__construct();
-			$this->load->helper('url_helper');
-			$this->load->model('user_model');
 			$this->load->library('session');
         }
 
