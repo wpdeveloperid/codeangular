48111ea512dd94ed0e4cd5c84665807ba6eb13fb
Khussal "Kus" Zamlahani <zamlahani@gmail.com>
2018-07-12 00:01:57 +0700

    check username exists & update password

---
+++ application/models/User_model.php
@@ -0,0 +1,21 @@
+<?php
+class User_model extends CI_Model {
+
+        public function __construct()
+        {
+			$this->load->database();
+        }
+        
+        public function check($username_input)
+        {
+			
+			$query = $this->db->get_where('user', array('username' => $username_input));
+			return $query->result();
+        }        
+        
+        public function update_password($username,$new_password){
+			$this->db->set('password', $new_password);
+			$this->db->where('username', $username);
+			return $this->db->update('user');
+        }
+}  
