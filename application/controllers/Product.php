fb8b12616eb61f43d1450604a8d7cec10000cb4b
Khussal "Kus" Zamlahani <zamlahani@gmail.com>
2018-07-12 21:36:31 +0700

    upload product image & resize

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -27,9 +27,59 @@ class Product extends CI_Controller {
 	}
 	
 	public function add(){
+		
+		$config['upload_path'] = './assets/img/upload/';
+		$config['allowed_types'] = 'jpg';
+		$config['file_name'] = 'upload.jpg';
+		$config['encrypt_name'] = TRUE;
+		$this->load->library('upload', $config);
+		
+		if(!$this->upload->do_upload('image')){
+			echo $this->upload->display_errors();
+		} else {
+			echo json_encode($this->upload->data());
+			$image_data = $this->upload->data();
+			$this->load->library('image_lib');
+			$w = $image_data['image_width']; // original image's width
+			$h = $image_data['image_height']; // original images's height
+			$n_w = 400; // destination image's width
+			$n_h = 300; // destination image's height
+			$s_r = $w / $h;
+			$n_r = $n_w / $n_h;
+			$config['source_image'] = $image_data['full_path'];
+			$config['maintain_ratio'] = FALSE;
+			$config['new_image'] = $image_data['file_path'].$image_data['raw_name'].'resized'.$image_data['file_ext'];
+			$new_path = $config['new_image'];
+			if($n_r > $s_r ){
+				$config['width'] = $w;
+				$config['height'] = round($w/$n_r);
+				$config['y_axis'] = round(($h - $config['height'])/2);
+				
+			} else {
+				$config['width'] = round($h * $n_r);
+				$config['height'] = $h;
+				$config['x_axis'] = round(($w - $config['width'])/2);
+			}
+			$this->image_lib->initialize($config);
+			$this->image_lib->crop();
+			$this->image_lib->clear();
+			
+			$config['source_image'] = $new_path;
+			$config['maintain_ratio'] = TRUE;
+			$config['width'] = $n_w;
+			$config['height'] = $n_h;
+			$this->image_lib->initialize($config);
+			if (!$this->image_lib->resize()){
+				echo $this->image_lib->display_errors();
+			} else {
+				echo "done";
+			}
+		}
+		
 		$data = array(
 			'name'=>$this->input->post('name'),
 			'price'=>$this->input->post('price'),
+			'image'=>$image_data['raw_name']
 		);
 		$this->product_model->add($data);
 	}
--- application/views/admin/addproduct.php
+++ application/views/admin/addproduct.php
@@ -2,9 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<?php echo form_open('product/add'); ?>
+<?php echo form_open_multipart('product/add'); ?>
 <?php echo form_input('name'); ?>
 <?php echo form_input(array('name'=>'price','type'=>'number')); ?>
+<?php echo form_upload(array('name'=>'image','accept'=>'.jpeg')); ?>
 <?php echo form_submit('submit','Add'); ?>
 <?php echo form_reset('reset','Reset'); ?>
 <?php echo form_close(); ?>
